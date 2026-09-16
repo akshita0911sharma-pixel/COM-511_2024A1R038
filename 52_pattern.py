ca2efa0ce9b897af9e88a9574a5f47542a547a30
akshita0911sharma-pixel <akshita0911sharma@gmail.com>
2026-09-16 11:56:41 +0530

    commit

---
+++ 52_pattern.py
@@ -0,0 +1,16 @@
+# Write a program to print a centered pyramid using stars.
+n = int(input("Enter the number of rows: "))
+
+for i in range(n):
+    # Print leading spaces
+    for j in range(n - i - 1):
+        print(" ", end="")
+
+    # Print stars
+    for k in range(2 * i + 1):
+        print("*", end="")
+
+    # Move to the next line
+    print()
+
+    
